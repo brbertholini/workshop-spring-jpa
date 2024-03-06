a4fa1e456ab006fabb30923c3e7f6c0ba67b6f97
Bruno Bertholini <brbertholini@gmail.com>
2024-03-06 17:30:40 -0300

    exception handling - update

--- src/main/java/com/brbertholini/springCourse/services/UserService.java
+++ src/main/java/com/brbertholini/springCourse/services/UserService.java
@@ -3,6 +3,7 @@ package com.brbertholini.springCourse.services;
 import com.brbertholini.springCourse.repositories.UserRepository;
 import com.brbertholini.springCourse.services.exceptions.DatabaseException;
 import com.brbertholini.springCourse.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.brbertholini.springCourse.entities.User;
@@ -43,9 +44,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
